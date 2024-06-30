6bd332b5e76483843e1b953bb30cfb974be301bf
Nereus-Nguyen <127203533+Nereus-Nguyen@users.noreply.github.com>
2024-06-30 20:53:31 +0700

    Update Bai_Tap_Lan2_03.py

--- Bai_Tap_Lan2_03.py
+++ Bai_Tap_Lan2_03.py
@@ -1,3 +1,4 @@
+#day la comment by toan
 def student_infomation (student_name, index_name, week_index, exercise_index):
     ten_file = f"{student_name}_{index_name}_Tuan{week_index}_Bai{exercise_index}.py"
     return ten_file
